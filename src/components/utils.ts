ec5e6c7deeb6fd90adffd3f2b433d33a72bcf4c6
Max Zakharzhevskiy <maxzz@msn.com>
2020-12-14 18:13:34 -0800

    feature: importing original wave. 2

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,56 +1,4 @@
-//#region old definitions
-
-enum CurveStyle {
-    "ios" = "ios",
-    "ios9" = "ios9",
-}
-
-export type Options = {
-    // The DOM container where the DOM canvas element will be added
-    container: HTMLElement;
-    // The style of the wave: `ios` or `ios9`
-    style?: CurveStyle;
-    //  Ratio of the display to use. Calculated by default.
-    ratio?: number;
-    // The speed of the animation.
-    speed?: number;
-    // The amplitude of the complete wave.
-    amplitude?: number;
-    // The frequency for the complete wave (how many waves). - Not available in iOS9 Style
-    frequency?: number;
-    // The color of the wave, in hexadecimal form (`#336699`, `#FF0`). - Not available in iOS9 Style
-    color?: string;
-    // The `canvas` covers the entire width or height of the container.
-    cover?: boolean;
-    // Width of the canvas. Calculated by default.
-    width?: number;
-    // Height of the canvas. Calculated by default.
-    height?: number;
-    // Decide wether start the animation on boot.
-    autostart?: boolean;
-    // Number of step (in pixels) used when drawed on canvas.
-    pixelDepth?: number;
-    // Lerp speed to interpolate properties.
-    lerpSpeed?: number;
-    // Curve definition override
-    curveDefinition?: ICurveDefinition[];
-};
-
-export type IiOS9CurveDefinition = {
-    supportLine?: boolean;
-    color: string;
-};
-
-export type IClassicCurveDefinition = {
-    attenuation: number;
-    lineWidth: number;
-    opacity: number;
-};
-
-export type ICurveDefinition = IiOS9CurveDefinition | IClassicCurveDefinition;
-
-//#endregion old definitions
-
+import { IClassicCurveDefinition } from "./wave";
 
 function getDefinition(): IClassicCurveDefinition[] {
     return [
@@ -104,7 +52,7 @@ type Params = {
     AMPLITUDE_FACTOR: number;
     ATT_FACTOR: number;
 };
-
+/*
 let params: Params = {
     ctrl: {
         color: 'rgb(25, 127, 30)',
@@ -127,7 +75,7 @@ let params: Params = {
     AMPLITUDE_FACTOR: 0.6,
     ATT_FACTOR: 4,
 };
-
+*/
 function globalAttFn(x: number, params: Params): number {
     return Math.pow(params.ATT_FACTOR / (params.ATT_FACTOR + Math.pow(x, params.ATT_FACTOR)), params.ATT_FACTOR);
 }
